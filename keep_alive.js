9b3b2d39a70ad4e8fccd6825bafc51926c52a213
Atharv Pathak <78468172+Atharv-Pathak-14@users.noreply.github.com>
2022-01-12 09:29:30 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 6969;
+const port = 8080;
 app.get('/', (req, res) => res.send('Harmony Alive!'));
 
 app.listen(port, () => console.log(`Harmony streaming to http://localhost:${port}`));
